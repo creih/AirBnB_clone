5aa7f2620bf4c83473d54c5170b55404c5fe5ec4
creih <iherr.9@gmail.com>
2024-03-10 21:45:05 +0200

    tasks tests

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -24,3 +24,38 @@ class TestBaseModel(unittest.TestCase):
                 self.model.__dict__
                 )
         self.assertEqual(str(self.model), exp_str)
+
+    def test_initialization(self):
+        obj = BaseModel()
+        self.assertIsNotNone(obj.id)
+        self.assertIsNotNone(obj.created_at)
+        self.assertIsNotNone(obj.updated_at)
+
+    def test_serialization_deserialization(self):
+        obj = BaseModel()
+        serialized_obj = obj.to_dict()
+        deserialized_obj = BaseModel(**serialized_obj)
+        self.assertEqual(obj.__dict__, deserialized_obj.__dict__)
+
+    def test_attribute_modification(self):
+        obj = BaseModel()
+        obj.name = "Test"
+        self.assertEqual(obj.name, "Test")
+
+    def test_equality(self):
+        obj1 = BaseModel()
+        obj2 = BaseModel()
+        self.assertNotEqual(obj1.id, obj2.id)
+
+    def test_validation(self):
+        obj = BaseModel()
+        obj.name = 123
+        self.assertNotEqual(obj.name, 123)
+
+    def test_edge_cases(self):
+        obj = BaseModel()
+        obj.name = ""
+        self.assertEqual(obj.name, "")
+
+if __name__ == '__main__':
+    unittest.main()
