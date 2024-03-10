ccfd5a57ee97e0e7131ad3bd888a5af7a6edc71e
creih <iherr.9@gmail.com>
2024-03-10 12:06:41 +0200

    filestorage.py all fixed up and documented

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,24 +4,29 @@ this file ni iyo kubika objects
 """
 import json
 
+
 class FileStorage:
-        __file_path = "file.json"
-            __objects = {}
+    __file_path = "file.json"
+    __objects = {}
 
-                def all(self):
-                            return self.__objects
+    def all(self):
+        """this all method niya displaying objects zose."""
+        return self.__objects
 
-                            def new(self, obj):
-                                        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-                                                self.__objects[key] = obj
+    def new(self, obj):
+        """this new method niya creating new objects."""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
-                                                    def save(self):
-                                                                with open(self.__file_path, 'w') as f:
-                                                                                json.dump(self.__objects, f)
+    def save(self):
+        """this save method niya keeping objects."""
+        with open(self.__file_path, 'w') as f:
+            json.dump(self.__objects, f)
 
-                                                                                    def reload(self):
-                                                                                                try:
-                                                                                                                with open(self.__file_path, 'r') as f:
-                                                                                                                                    self.__objects = json.load(f)
-                                                                                                                                            except FileNotFoundError:
-                                                                                                                                                            pass
+    def reload(self):
+        """this reload method niya rerunning objects nanone."""
+        try:
+            with open(self.__file_path, 'r') as f:
+                self.__objects = json.load(f)
+        except FileNotFoundError:
+            pass
