ac2b9344dcab5034159ad4d3d94227a5f9218be4
creih <iherr.9@gmail.com>
2024-03-10 21:09:12 +0200

    task 8 typo in naming

--- models/User.py
+++ models/User.py
@@ -7,7 +7,7 @@ class User(BaseModel):
     """the actual user instance inheriting the basmodel class """
 
     def __init__(self):
-        """the initialisation of user class happens here to thoses wondering"""
+        """the initialisation of user class happens here to those wondering"""
         super().__init__(self, *ars, **kwargs)
         self.email = kwargs.get('email', "")
         self.password = kwargs.get('password', "")
