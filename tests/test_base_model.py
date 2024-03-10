32dc0a8be8dd698f5450b52f7dc116fa61d226b3
creih <iherr.9@gmail.com>
2024-03-10 06:27:30 -0700

    returned to how it was

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -16,6 +16,7 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(self.model.id, str)
         self.assertIsInstance(self.model.created_at, datetime)
         self.assertIsInstance(self.model.updated_at, datetime)
+
     def test_str_method(self):
         """ test the str method """
         exp_str = "[BaseModel] ({}) {}".format(
