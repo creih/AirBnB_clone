0a7dc69e39d0994ae14ec81e63606482e3ffbfb5
creih <iherr.9@gmail.com>
2024-03-11 20:37:02 +0200

    task 8 for the user file

---
+++ models/user.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+this is the User class that implements user methods as the
+task 8 demands.
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    pass
